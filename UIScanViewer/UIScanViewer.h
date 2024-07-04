eb36114669953c78f342af62445a19abeed77d04
Milad <milad.heidari@me.com>
2024-07-04 14:46:37 +0330

    Initial Project

---
+++ UIScanViewer/UIScanViewer.h
@@ -0,0 +1,18 @@
+//
+//  UIScanViewer.h
+//  UIScanViewer
+//
+//  Created by Milad on 7/4/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIScanViewer.
+FOUNDATION_EXPORT double UIScanViewerVersionNumber;
+
+//! Project version string for UIScanViewer.
+FOUNDATION_EXPORT const unsigned char UIScanViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIScanViewer/PublicHeader.h>
+
+
