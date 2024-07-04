abdc25dc34dfa07cbec8856c6206dddaa44c8b3f
Milad <milad.heidari@me.com>
2024-07-04 14:33:16 +0330

    Initial Commit

---
+++ OpalAiTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpalAiTestApp
+//
+//  Created by Milad on 7/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
